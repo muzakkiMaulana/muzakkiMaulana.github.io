8f901e46c1da8db9e8c2f1e256e019f6c9359f5b
muzakki <muzakki43527@gmail.com>
2022-05-21 06:55:42 +0700

    json dummy, tailwind css navbar

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,61 +1,93 @@
-import React,{Component} from 'react';
- 
+import React, { Component } from "react";
+
 class Nav extends Component {
-  constructor(load_Api){
-    super()
-    this.state= {
-            link : '/home',
-            text : [
-                1
-            ],
-            load_Api: load_Api,
-            data: false,
-        }
+  constructor(load_Api, onNameChange) {
+    super();
+    this.state = {
+      // link: "/home",
+      // text: [1],
+      load_Api: load_Api,
+      data: false,
+    };
   }
- 
+
   componentDidMount() {
     // this.phoneData = [
     //     { name: "iPhone X", price: 10000000, discount: 50 },
     //     { name: "Oppo Find X", price: 14000000, discount: 30 },
     //     { name: "Redmi Note X", price: 5000000, discount: 42 },
-    //   ];      
-     
-    console.log('load');
+    //   ];
+
+    // this.props.onNameChange(1);
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) =>
+        this.setState({
+          data: json,
+          load_Api: 1,
+        })
+      );
+
+    // console.log(this.state);
   }
 
-
-  goPremium(){
-      const count = this.state.text.length + 1;
-      const arr = this.state.text;
-      arr.push(count)
-
-    this.setState({
-      text:arr,
-      data: count > 3
-    })
-    console.log(this.state.load_Api);
+  goPremium() {
+    //   const count = this.state.text.length + 1;
+    //   const arr = this.state.text;
+    //   arr.push(count)
+    // this.setState({
+    //   text:arr,
+    //   data: count > 3
+    // })
+    // console.log(this.state.load_Api);
   }
 
   render() {
+    // console.log(this.phoneData);
+    // return (
+    //     <div>
+    //         {this.state.text.map((data, i) =>{
+    //             return data
+    //         })}
+    //         <div>
+    //             apakah ganjil ?
+    //         {
+    //             this.state.data ? 'tidak' : 'iya'
+    //         }
+    //         </div>
+    //       <button onClick={() => this.goPremium()}>
+    //         Go Premium
+    //       </button>
+    //     </div>
+    // );
 
-      console.log(this.phoneData);
-      return (
-          <div>
-              {this.state.text.map((data, i) =>{
-                  return data
+    if (!this.state.data) {
+      return <div>loading</div>;
+    }
+    console.log(this.state.data);
+
+    return (
+      <nav className="navbar">
+        <div className="container">
+          <div className="list-menu">
+            <ul>
+              {this.state.data.map((data, i) => {
+                const active = (i == 4 ? "active": "");
+                console.log(active, i);
+                if(i > 3){
+                  return (
+                   <li>
+                     <a className={active}>{data.name}</a>
+                   </li>
+                  );
+                }
               })}
-              <div>
-                  apakah ganjil ? 
-              {
-                  this.state.data ? 'tidak' : 'iya' 
-              }
-              </div>
-            <button onClick={() => this.goPremium()}>
-              Go Premium
-            </button>
+            </ul>
           </div>
-      ); 
+        </div>
+      </nav>
+    );
   }
 }
- 
+
 export default Nav;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,129 @@
-import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Suspense, useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 // import Navigation from '../src/components/nav';
-const Navigation = React.lazy(() => import('../src/components/nav'));
-import './style/style.css';
+const Navigation = React.lazy(() => import("../src/components/nav"));
+import "./style/style.css";
 
 const load_Api = {
+  test: 1,
+};
 
-    test:1
+// const App = () => {
+//   const [userDetails, setUserDetails] = useState({});
 
-}
-class Header extends React.Component {
+//   useEffect(() => {
+//     // fetchUserDetails().then(setUserDetails)
+//   }, []);
+//   console.log(userDetails);
 
-    
+//   if (userDetails == 1) {
+//   console.log(userDetails);
 
-    render(){
-      return (
-        <div>
-          <h1>Tutorial Reactjs untuk Pemula</h1>
-          <h2>Panduan step-by-step belajar Reactjs</h2>
-          <p>Membuat komponen dengan class</p>
-          <Suspense fallback={<div>Loading...</div>}>
-            <Navigation load_Api={load_Api} ></Navigation>
-          </Suspense>
-        </div>
-        // <h1 className='text-red-800 font-bold underline bg-slate-50'>adsdasd</h1>
+//   }
 
-      );
-    }
+//   const handleNameChange = name => {
+//     userDetails = name
+//   }
 
+//   return (
+//     // <div>
+//     //   <h1>Tutorial Reactjs untuk Pemula</h1>
+//     //   <h2>Panduan step-by-step belajar Reactjs</h2>
+//     //   <p>Membuat komponen dengan class</p>
+//     //   <Suspense fallback={<div>Loading...</div>}>
+//     //     <Navigation load_Api={load_Api}></Navigation>
+//     //   </Suspense>
+//     // </div>
+//     // <h1 className='text-red-800 font-bold underline bg-slate-50'>adsdasd</h1>
+//     <div>
+//       <h1>Tutorial Reactjs untuk Pemula</h1>
+//       <h2>Panduan step-by-step belajar Reactjs</h2>
+//       <p>Membuat komponen dengan class</p>
+//       <Suspense fallback={<div>Loading...</div>}>
+//         <Navigation userDetails={userDetails} onNameChange={handleNameChange}></Navigation>
+//       </Suspense>
+//     </div>
+//   );
+// };
 
-}
+// const UserWelcome = () => {
 
+//   useEffect(() => {
+//     loadData.nav().then(
+//       data => console.log(data)
+//     );
+//   }, []);
 
+//   // const userDetails = loadData.nav()
+//   // console.log(userDetails);
+//   // code to render welcome message
 
-  ReactDOM.render(<Header/>, document.getElementById("root"));
+//   return(
+//     <ul>
+
+//   </ul>
+//   )
+// }
+// class App extends React.Component {
+
+//   useEffect(() => {
+//     fetchUserDetails().then(setUserDetails)
+//   }, []),
+//   render() {
+//     const data= loadData.nav()
+//     console.log(data);
+//     return (
+//       <div>
+//         <h1>Tutorial Reactjs untuk Pemula</h1>
+//         <h2>Panduan step-by-step belajar Reactjs</h2>
+//         <p>Membuat komponen dengan class</p>
+//         <Suspense fallback={<div>Loading...</div>}>
+//           <Navigation load_Api={load_Api}></Navigation>
+//         </Suspense>
+//       </div>
+//       // <h1 className='text-red-800 font-bold underline bg-slate-50'>adsdasd</h1>
+//     );
+//   }
+// }
+
+class App extends React.Component {
   
+  state = {
+    userDetails: "",
+    setUserDetails: "",
+  }
+
+  handleNameChange = (name) => {
+    console.log(name);
+    this.setState({userDetails:name});
+  }
+
+  render() {
+    if (this.state.userDetails == 1) {
+      console.log(this.state);
+    }
+    return (
+      // <div>
+      //   <h1>Tutorial Reactjs untuk Pemula</h1>
+      //   <h2>Panduan step-by-step belajar Reactjs</h2>
+      //   <p>Membuat komponen dengan class</p>
+      //   <Suspense fallback={<div>Loading...</div>}>
+      //     <Navigation load_Api={load_Api}></Navigation>
+      //   </Suspense>
+      // </div>
+      // <h1 className='text-red-800 font-bold underline bg-slate-50'>adsdasd</h1>
+      
+      <div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Navigation
+            userDetails={this.state.userDetails}
+            onNameChange={this.handleNameChange}
+          ></Navigation>
+        </Suspense>
+      </div>
+      
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
